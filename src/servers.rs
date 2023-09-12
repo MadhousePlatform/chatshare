056c84f64178f7e73692d0fcd61d5ebfb45071e0
Steve Goode <steve@madhouseminers.com>
2023-09-12 15:23:08 +0000

    Added docker and workflow

--- src/servers.rs
+++ src/servers.rs
@@ -1,3 +1,4 @@
+use reqwest::header::{HeaderMap, AUTHORIZATION};
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
@@ -6,15 +7,34 @@ pub struct ServerInfo {
     pub name: String,
 }
 
+#[derive(Deserialize, Debug)]
+pub struct ServerObject {
+    attributes: ServerInfo,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Resp {
+    data: Vec<ServerObject>,
+}
+
 pub async fn get_servers() -> Result<Vec<ServerInfo>, reqwest::Error> {
     let client = reqwest::Client::new();
-    let result = client
-        .get(std::env::var("SERVER_API").unwrap())
-        .send()
-        .await;
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        AUTHORIZATION,
+        format!("Bearer {}", std::env::var("SERVER_KEY").unwrap())
+            .parse()
+            .unwrap(),
+    );
 
-    match result {
-        Ok(result) => result.json().await,
-        Err(e) => Err(e),
-    }
+    let result = client
+        .get(format!(
+            "{}/api/application/servers",
+            std::env::var("SERVER_API").unwrap()
+        ))
+        .headers(headers)
+        .send()
+        .await?;
+    let response = result.json::<Resp>().await.unwrap();
+    Ok(response.data.into_iter().map(|d| d.attributes).collect())
 }
